7855f348c49ffc009325ff5116c78a3af26b51be
Knaito163 <deen.sea210@gmail.com>
2016-09-14 10:00:29 +0900

    yesterday

---
+++ public/example.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('.category tr td').on('click', function(){
+    $('#listener').toggleClass('hidden');
+  });
+});
+
+var audio = new Audio();
+audio.src = "audios/sample.wav";
+
+var onMusicClick = function(){
+  console.log("音楽スタート予定");
+  var img = document.getElementById("mid");
+  var imgPath = img.getAttribute("src");
+  if(imgPath == "images/Start.jpg"){
+    document.getElementById("mid").src = "images/stop.png";
+    audio.play();
+    imgPath = img.getAttribute("src");
+  }else if(imgPath == "images/stop.png"){
+    document.getElementById("mid").src = "images/Start.jpg";
+    audio.pause();
+    imgPath = img.getAttribute("src");
+  }
+};
